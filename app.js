3965b1d0f261a1205fd8415151e176494d14e82a
Aldair Gutierrez <aldairgguer@gmail.com>
2025-02-27 10:10:49 -0500

    feat: reestructurar el servicio de envío de correos y agregar soporte para MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 import express, { json } from "express";
-import emailRoutes from "./src/routes/emailRoutes.js";
+import mongoose from "mongoose";
+import sendEmailRoutes from "./src/routes/sendEmailRoutes.js";
 import dotenv from "dotenv";
+import { dbUrl } from "./src/config/database.js";
 dotenv.config();
 
 const app = express();
 app.use(json());
 
-app.use("/messaging-service", emailRoutes);
+app.use("/messaging-service/email", sendEmailRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("🔥 Conectado a MongoDB"))
+  .catch((err) => console.error("❌ Error de conexión:", err));
